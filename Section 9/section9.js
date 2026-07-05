472e61c17f4b71658a7c3bba6d798699d4981010
Israel Bernardino <israelbernardino2006@gmail.com>
2026-07-05 16:00:34 -0300

    desestruturando arrays

---
+++ Section 9/section9.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+};
+
+//Desestruturando arrays
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second)
+
+// Pulando elementos da array
+// const [first, , second] = restaurant.categories;
+// console.log(first, second);
