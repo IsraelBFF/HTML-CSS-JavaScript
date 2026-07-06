17d4d96feff0aad61c0c4efac1201d080cce1ad0
Israel Bernardino <israelbernardino2006@gmail.com>
2026-07-06 14:19:05 -0300

    Destructuring arrays

--- Section 9/section9.js
+++ Section 9/section9.js
@@ -6,9 +6,12 @@ const restaurant = {
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
 
-//Desestruturando arrays
+// ##=== Destructuring arrays  ===## //
 
 // const [first, second] = restaurant.categories;
 // console.log(first, second)
@@ -16,3 +19,31 @@ const restaurant = {
 // Pulando elementos da array
 // const [first, , second] = restaurant.categories;
 // console.log(first, second);
+//-------------------------------------------------------
+
+// === Switching variables === //
+// let [first, , second] = restaurant.categories;
+// console.log(first, second);
+
+// [first, , second] = [second, , first];
+
+// console.log(first, second);
+//-------------------------------------------------------
+
+// === Receives 2 return values from a function === //
+// const [first, second] = restaurant.order(2, 0);
+// console.log(first, second);
+//-------------------------------------------------------
+
+// === Nested destructuring === //
+// const aninhada = [2, 3, [5, 6]];
+// const [a, , [b, c]] = aninhada;
+// console.log(a, b, c);
+//-------------------------------------------------------
+
+// === Default valures === //
+// const [a = 0, b = 0, c = 0] = [12, 10];
+// console.log(a, b, c);
+
+
+
